f8ae2dfd5a98ce72031527c1b1e7f88016461c07
ironmike718 <137319205+ironmike718@users.noreply.github.com>
2026-02-13 03:32:42 +0000

    created hello world py script

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+
+# the hash is a one line comment, nothing here executes
+
+"""
+the triple quotes are a multi-line comment
+see
+believe me
+anything inside will not execute
+"""
+
+# this will print to the screen
+print("Hello World!")
+
