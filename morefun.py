38fa801f21246f44ff8c949f4e6acd8a58e6c7da
ironmike718 <137319205+ironmike718@users.noreply.github.com>
2026-02-19 01:49:11 +0000

    more stuff

---
+++ morefun.py
@@ -0,0 +1,25 @@
+"""
+name = "Gwen"
+print("My friend is: " + name)
+
+# welcome to fstrings
+print(f"My friend is: {name}")
+
+
+
+
+user_name = input("What is your name? ")
+num_1 = input("Give me a number ")
+num_2 = input("Give me another number ")
+
+total = float(num_1) + float(num_2)
+#str(total)
+
+print(f"Your name is: {user_name} and the total is {total}")
+
+"""
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
