921131db1932147029cdb834afd6c1901223e3b2
ironmike718 <137319205+ironmike718@users.noreply.github.com>
2026-02-20 01:53:24 +0000

    more python fun

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+# this is a dictionary exercise
+teacher = {"name":"mike", "age":"48"}
+
+# add a new dictionary element called city with a value of boston
+
+# print a sentence to the screen using f-strings that says
+# mike is 48 and lives in boston
+
