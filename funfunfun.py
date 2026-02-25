f983f923e0b45322be55c4c8d1464c722c893dd4
ironmike718 <137319205+ironmike718@users.noreply.github.com>
2026-02-25 02:51:33 +0000

    mo pythone

---
+++ funfunfun.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+userInput = input("What is your name? ")
+
+if userInput == "mike":
+    role = "teacher"
+else:
+    role = "student"
+
+counter = 1
+while counter <= 3:
+    if role == "teacher":
+        print("Life is great!")
+    else:
+        print("Life is tough")
+    counter +=1
+
+# opening a file w/ python
+
+myNameFile = open("names.txt")
+
+#print(myNameFile.read())
+
+counter = 1
+for line in myNameFile:
+    aLine = line.strip()
+    if aLine == "Brandon":
+        print(f"the name on line-{counter} is {aLine}")
+    else:
+        print(f"the name is not Brandon, it's {aLine}")
+    counter += 1
+
+
+
