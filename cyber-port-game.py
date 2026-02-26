4b0214abc9be28958f2d791f3cdfc8bdaddcd773
ironmike718 <137319205+ironmike718@users.noreply.github.com>
2026-02-26 02:05:56 +0000

    final commit

--- cyber-port-game.py
+++ cyber-port-game.py
@@ -11,14 +11,14 @@ import random, os, time, sys
 # variables
 
 banner = "\n\n ### Cyber Port Number Guessing Game ### \n\n"
-svc_names = ["SSH", "HTTP", "HTTPS", "Telnet", "RDP", "VNC", "POP3", "SMTP", "DNS", "DHCP"]
-port_nums = [22, 80, 443, 23, 3389, 5900, 110, 25, 53, 67]
+svc_names = ["SSH", "HTTP", "HTTPS", "Telnet", "RDP", "VNC", "POP3", "SMTP", "DNS", "DHCP", "FTP"]
+port_nums = [22, 80, 443, 23, 3389, 5900, 110, 25, 53, 67, 21]
 score = 0
 
 # generate random integer list from 0 to 9
 # use them to access the svc_names and port_nums elements
 
-ran_int_list = random.sample(range(0, 10), 10)
+ran_int_list = random.sample(range(0, 11), 11)
 
 # print the banner
 
@@ -33,10 +33,10 @@ for num in ran_int_list:
     user_guess = int(user_guess)
     #
     if user_guess == port_nums[num]:
-        print(f"correct -- {svc_names[num]} run on port # {port_nums[num]}\n")
-        score =+ 1
+        print(f"correct -- {svc_names[num]} runs on port # {port_nums[num]}\n")
+        score += 1
     else:
-        print(f"sorry -- {svc_names[num]} run on port # {port_nums[num]}\n")
+        print(f"sorry -- {svc_names[num]} runs on port # {port_nums[num]}\n")
 
 # calculate the score
 
