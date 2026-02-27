4d9ceab51fac1422c1dfe679cb529a3a6f51f94a
ironmike718 <137319205+ironmike718@users.noreply.github.com>
2026-02-27 02:28:07 +0000

    new bash

--- userinput.sh
+++ userinput.sh
@@ -2,13 +2,22 @@
 
 # get user input
 
-echo -n "What is your favorite food? "
-read favFood
+echo -n "Give me a number from 1 to 20: "
+read num1
 
-echo -n "What is your favorite drink? "
-read favDrink
+echo -n "Give me another number from 1 to 20: "
+read num2
+
+# use an if statement
+if [ ${num1} -gt ${num2} ]; then
+  echo -e "${num1} is > ${num2}\n"
+elif [ ${num1} -lt ${num2} ]; then
+  echo -e "${num2} is > ${num1}\n"
+else
+  echo -e "i don't know anything about numbers!\n"
+fi
 
 # print user input to the screen
-echo -e "\nYou told me your favorite food is ${favFood} and favorite drink is ${favDrink}\n"
+# echo -e "\nYou told me your favorite food is ${favFood} and favorite drink is ${favDrink}\n"
 
 exit 0
